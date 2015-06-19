1a41872ac055368439869a4100aaad9ffc7bed20
Federico <federico@alumni.ubc.ca>
2015-06-19 16:45:11 -0700

    Added no-ip dns and automatic redirect if no instagram login.

--- Touristy/home/views.py
+++ Touristy/home/views.py
@@ -13,7 +13,7 @@ session_opts = {
 CONFIG = {
     'client_id': '8d2ddb72ef774dc6a472a4a2090ebbe1',
     'client_secret': 'b1871feaade14048b479907e02784883',
-    'redirect_uri': 'http://128.189.139.253/'
+    'redirect_uri': 'http://touristy.no-ip.org/'
 }
 
 unauthenticated_api = InstagramAPI(**CONFIG)
